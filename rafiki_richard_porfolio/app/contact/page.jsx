796b62e773ce179f329e5134d3a9958691ab301c
ramus <ramussiz29@gmail.com>
2024-08-26 09:25:12 +0100

    feat: add form in contact page

--- rafiki_richard_porfolio/app/contact/page.jsx
+++ rafiki_richard_porfolio/app/contact/page.jsx
@@ -1,7 +1,85 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { motion } from "framer-motion";
+import { Select, SelectItem, SelectTrigger, SelectContent, SelectGroup, SelectValue, SelectLabel  } from "@radix-ui/react-select";
+import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa";
+import { useState } from "react";
+
+const infos=[
+    {
+        icon: <FaPhoneAlt/>,
+        label: "Phone",
+        value: "(+234) 970 361 929"
+    },
+    {
+        icon: <FaEnvelope/>,
+        label: "Email",
+        value: "rafiki.richard@grafikirdc.com"
+    },
+    {
+        icon: <FaMapMarkerAlt/>,
+        label: "Location",
+        value: "Kinshasa, RDC"
+    }
+]
+
 export default function Contact() {
+    const [selectedValue,setSelectedValue]=useState('');
 
     return (
-        <div>Contact</div>
+        <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{duration:2.4, ease:"easeIn"}}} className="py-6">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-[30px]">
+                    <div className="xlh-[50%] order-2 xl:order-none ">
+                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                            <h3 className="text-4xl text-accent">Travaillons ensemble!</h3>
+                            <p className="text-white/60">Une Collaboration pour créer un impact numérique durable.</p>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input
+                                type="firstname"
+                                placeholder="Nom"
+                                />
+                                <Input
+                                type="lastname"
+                                placeholder="Prénom"
+                                />
+                                <Input
+                                type="email"
+                                placeholder="Email"
+                                />
+                                 <Input
+                                type="phone"
+                                placeholder="Téléphone"
+                                />
+                            </div>
+
+                            <Select onValueChange={setSelectedValue} value={setSelectedValue}>
+                                <SelectTrigger className="w-full bg-primary border border-white/60 text-white/60 hover:border-white focus:border-accent rounded-md">
+                                    {selectedValue || "Pour quel service ?"}
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup className="bg-primary p-4 border border-white/60 text-white/60 rounded-sm ">
+                                        <SelectLabel className="text-white/80" >Selectionnez un service</SelectLabel>
+                                        <SelectItem value="Développemnt Web" className="focus:bg-accent focus:text-primary pl-3">Développemnt Web</SelectItem>
+                                        <SelectItem value="UI/UX"  className="focus:bg-accent focus:text-primary pl-3">UI/UX</SelectItem>
+                                        <SelectItem value="Logo Design"  className="focus:bg-accent focus:text-primary pl-3">Logo Design</SelectItem>
+                                        <SelectItem value="SEO"  className="focus:bg-accent focus:text-primary pl-3">SEO</SelectItem>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </form>
+                    </div>
+                    <div className="flex-1 flex items-center xl:justify-center order-1 xl:order-none mb-8 xl:mb-0">Infos</div>
+
+                </div>
+
+            </div>
+
+        </motion.section>
     )
     
 }
