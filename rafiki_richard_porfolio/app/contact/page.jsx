538549d4092b25fccef570cada41202eb31d6b11
ramus <ramussiz29@gmail.com>
2024-08-26 12:02:24 +0100

    feat contact page: custorm design

--- rafiki_richard_porfolio/app/contact/page.jsx
+++ rafiki_richard_porfolio/app/contact/page.jsx
@@ -32,7 +32,7 @@ export default function Contact() {
         <motion.section initial={{opacity:0}} animate={{opacity:1, transition:{duration:2.4, ease:"easeIn"}}} className="py-6">
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
-                    <div className="xlh-[50%] order-2 xl:order-none ">
+                    <div className="xl:w-[54%] order-2 xl:order-none ">
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Travaillons ensemble!</h3>
                             <p className="text-white/60">Une Collaboration pour créer un impact numérique durable.</p>
@@ -85,7 +85,7 @@ export default function Contact() {
                                     <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
                                        <div className="text-[28px]">{info.icon}</div>
                                     </div>
-                                    <div>
+                                    <div className="flex-1">
                                     <p className="text-white/60">{info.label}</p>
                                     <h3 className="text-xl">{info.value}</h3>
                                     </div>
