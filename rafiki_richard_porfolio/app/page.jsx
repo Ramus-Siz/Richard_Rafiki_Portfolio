e7fa7b93aa3898e06b72dbbcde6cf604940d7a27
ramus <ramussiz29@gmail.com>
2024-08-26 12:58:50 +0100

    feat: dowload resume button add resume file

--- rafiki_richard_porfolio/app/page.jsx
+++ rafiki_richard_porfolio/app/page.jsx
@@ -15,10 +15,12 @@ export default function Home() {
             <h1 className="h1 mb-6 text-4xl"><span className="text-accent">Rafiki Richard</span></h1>
             <p className="max-w-[500px] mb-9 text-white/80">En réalisant les missions confiées, ma principale motivation reste celle de contribuer à la croissance et à la réussite de l'entreprise qui m'emploie. </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+            <a href="../assets/resume/Resume__Richard_Rafiki.pdf" download>
+              <Button  variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <span>Télécharger le cv</span>
                 <FiDownload className="text-lg"/>
               </Button>
+            </a>
               <div className="mb-8 xl:mb-0 ">
                 <Social containerStyles={"flex gap-6"} iconStyles={"w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"}/>
               </div>
