fd42246d9e0b2f450e524bfcfdcd18fecace0409
ramus <ramussiz29@gmail.com>
2024-08-28 09:00:19 +0100

    build: deployement

--- rafiki_richard_porfolio/app/contact/page.jsx
+++ rafiki_richard_porfolio/app/contact/page.jsx
@@ -48,6 +48,7 @@ export default function Contact() {
             .catch((error) => {
                 console.error('FAILED...', error);
             });
+
     };
 
     return (
@@ -55,6 +56,7 @@ export default function Contact() {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-[30px]">
                     <div className="xl:w-[54%] order-2 xl:order-none">
+                        {/*contact form */}
                         <form ref={formRef} onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
                             <h3 className="text-4xl text-accent">Travaillons ensemble!</h3>
                             <p className="text-white/60">Une Collaboration pour créer un impact numérique durable.</p>
