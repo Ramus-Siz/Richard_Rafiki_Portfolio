e38f6673d88341588df7f3a12493872754c54b5b
ramus <ramussiz29@gmail.com>
2024-08-28 18:16:13 +0100

    feat Mobilenav: add state for closing sheet when clicking a link

--- rafiki_richard_porfolio/components/MobileNav.jsx
+++ rafiki_richard_porfolio/components/MobileNav.jsx
@@ -1,59 +1,68 @@
 "use client";
-import { Sheet,SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {CiMenuFries} from "react-icons/ci";
+import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
-const links=[
-    {
-        name:'home',
-        path:"/",
-    },
-
-    {
-        name:'services',
-        path:"/services",
-    },
-    {
-        name:'resume',
-        path:"/resume",
-    },
-    {
-        name:'contact',
-        path:"/contact",
-    },
-
-    {
-        name:'Grafiki RDC',
-        path:"https://grafikirdc.com",
-    }
-
-] 
+const links = [
+  {
+    name: 'home',
+    path: "/",
+  },
+  {
+    name: 'services',
+    path: "/services",
+  },
+  {
+    name: 'resume',
+    path: "/resume",
+  },
+  {
+    name: 'contact',
+    path: "/contact",
+  },
+  {
+    name: 'Grafiki RDC',
+    path: "https://grafikirdc.com",
+  }
+];
 
 export default function MobileNav() {
-    const pathname=usePathname();
-    return(
-        <Sheet>
-            <SheetTrigger className="flex justify-center items-center">
-                <CiMenuFries className="text-[32px] text-accent" />
-            </SheetTrigger>
-            <SheetContent className="flex flex-col">
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
-                <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link href="/">
-                        <h1 className="text-4xl font-semibold">
-                            Rafiki<span className="text-accent">.</span>
-                        </h1>
-                    </Link>
-                </div>
-                <nav className="flex flex-col justify-center items-center gap-8">
-                    {links.map((link, index)=>{
-                        return (<Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}>{link.name}</Link>
-                        );
-                    })}
-                </nav>
-            </SheetContent>
-        </Sheet>
-        
-    ) ;
+  const handleLinkClick = () => {
+    // Ferme le Sheet après le clic sur un lien
+    setIsOpen(false);
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/" onClick={handleLinkClick}>
+            <h1 className="text-4xl font-semibold">
+              Rafiki<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => (
+            <Link 
+              href={link.path} 
+              key={index} 
+              className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all`}
+              onClick={handleLinkClick} // Ajoutez la fonction de gestion de clic ici
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 }
